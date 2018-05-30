0c01a3deeed0db95198f6782bc2223c856e35c78
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-05-30 14:25:28 +0100

    Add script to start local GraphQL server

--- server/index.js
+++
@@ -1,6 +0,0 @@
-const { GraphQLServer } = require('graphql-yoga');
-
-const resolvers = {};
-
-const server = new GraphQLServer({ typeDefs: './schema.graphql', resolvers });
-server.start(() => console.log('GraphQL server on localhost:4000'));

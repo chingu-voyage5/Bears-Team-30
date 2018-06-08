45d32196ec464917cfea389f3e38abc3a8f45acc
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2018-06-08 20:06:57 +0100

    Remove model timestamp and setup to use own

--- server/src/models/order.js
+++ server/src/models/order.js
@@ -3,20 +3,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const orderSchema = new Schema(
-  {
-    name: String,
-    remark: String,
-    menuItems: Array,
-    totalDishes: Number,
-    discountCards: Number,
-    total: Number,
-    qty: Number,
-    scannedAt: Date,
-    deliveredAt: Date,
-    status: String
-  },
-  { timestamps: { orderedAt: 'createdAt', updatedAt: 'updatedAt' } }
-);
+const orderSchema = new Schema({
+  name: String,
+  remark: String,
+  menuItems: Array,
+  totalDishes: Number,
+  discountCards: Number,
+  total: Number,
+  qty: Number,
+  createdAt: String,
+  scannedAt: String,
+  deliveredAt: String,
+  status: String
+});
 
+// "[{\"foodId\":\"5b1aa7ec7ffd9a121d5030ef\",\"name\":\"chicken appel\",\"category\":\"adult\",\"price\":16,\"qty\":2}]";
 module.exports = mongoose.model('Order', orderSchema);

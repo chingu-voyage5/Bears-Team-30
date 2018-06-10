09833132addf9496a8a2d33257836e3076dbd971
is343 <anz17n@gmail.com>
2018-06-10 18:20:56 +0900

    edit createOrderMutation variables

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 import EntrancePage from '../../pages/EntrancePage'
+import OrderPage from '../../pages/OrderPage'
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+})
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <Switch>
-          <Route exact path='/' component={EntrancePage} />
-          <Route path='/entrance' component={EntrancePage} />
-        </Switch>
-      </div>
+      <ApolloProvider client={client}>
+        <div className='App'>
+          <Switch>
+            <Route exact path='/' component={OrderPage} />
+            <Route path='/entrance' component={EntrancePage} />
+          </Switch>
+        </div>
+      </ApolloProvider>
     )
   }
 }

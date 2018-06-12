f11f5bdd0a8deed57c44b5475d1de56365add25d
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2018-06-12 10:02:56 +0800

    Add ContentArea Component

---
+++ client/src/components/ContentArea.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const ContentArea = styled.main`
+  width: 1200px;
+  max-width: 100%;
+  padding: 60px 15px;
+  margin: 0 auto;
+`
+
+export default ContentArea

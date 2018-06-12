1fdebd9a577e65bd03e2e6644dca0ed2bfd25c3e
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2018-06-12 11:24:07 +0800

    Move Apollo Setup to src/index

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import fontawesome from '@fortawesome/fontawesome'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
 import App from './components/App'
 import BaseStyles from './BaseStyles'
@@ -12,10 +14,16 @@ import registerServiceWorker from './registerServiceWorker'
 
 fontawesome.library.add(faListUl, faTable)
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+})
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root')
 )
 registerServiceWorker()
